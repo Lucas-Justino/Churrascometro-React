705c424ff3f03f6bafb61b7b2bd5e0e361081098
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-03-10 00:31:28 -0300

    novas interfaces

---
+++ src/interfaces/IAllInformations.interface.ts
@@ -0,0 +1,13 @@
+interface IAllInformations {
+  id: string;
+  data: string;
+  totalPessoas: number;
+  carnes: number;
+  paoDeAlho: number;
+  carvao: number;
+  refrigerantes: number;
+  cerveja: number;
+  criancas: number;
+  mulheres: number;
+  homens: number;
+}
---
+++ src/interfaces/IData.interface.ts
@@ -0,0 +1,10 @@
+interface IData {
+  id: string;
+  data: string;
+  totalPessoas: number;
+  carnes: number;
+  paoDeAlho: number;
+  carvao: number;
+  refrigerantes: number;
+  cerveja: number;
+}
