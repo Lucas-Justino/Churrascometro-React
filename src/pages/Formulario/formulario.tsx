dec20e225616b08b9ebaa90c9a5e32d594e336f2
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-03-12 10:27:55 -0300

    teste db

--- src/pages/Formulario/formulario.tsx
+++ src/pages/Formulario/formulario.tsx
@@ -3,9 +3,9 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { Input } from "../../components/Input/input";
 import Navbar from "../../components/Navbar/navbar";
-import './formulario.css'
-import Imagem from '../../assets/cozinheiro1.png'
-import { v4 as uuidv4 } from 'uuid';
+import "./formulario.css";
+import Imagem from "../../assets/cozinheiro1.png";
+import { v4 as uuidv4 } from "uuid";
 import { useStore } from "../../zustand/useForm/useForm.zustand";
 
 interface Inputs {
@@ -17,11 +17,7 @@ interface Inputs {
 
 const schema = yup
   .object({
-    homens: yup
-      .number()
-      .min(0)
-      .integer()
-      .required(),
+    homens: yup.number().min(0).integer().required(),
     data: yup
       .date()
       .min(
@@ -29,29 +25,25 @@ const schema = yup
         "Data inválida, o evento precisa ser marcado futuramente"
       )
       .required("Erro na data"),
-    mulheres: yup
-      .number()
-      .min(0)
-      .integer()
-      .required(),
-    criancas: yup
-      .number()
-      .min(0)
-      .integer()
-      .required(),
+    mulheres: yup.number().min(0).integer().required(),
+    criancas: yup.number().min(0).integer().required(),
   })
   .required();
 
 export default function Formulario() {
-  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
     resolver: yupResolver(schema),
     mode: "onChange",
   });
 
-  const enviarDados = useStore(state => state.enviarDados); // Função do store para enviar os dados
+  const enviarDados = useStore((state) => state.enviarDados); // Função do store para enviar os dados
 
   const onSubmit = (data: Inputs) => {
-    const date = data.data
+    const date = data.data;
     const totalPessoas = data.criancas + data.mulheres + data.homens;
     const carnes = Math.ceil(
       data.criancas * 0.4 + data.mulheres * 0.32 + data.homens * 0.2
@@ -60,7 +52,7 @@ export default function Formulario() {
     const carvao = data.homens + data.mulheres + data.criancas;
     const refrigerantes = Math.ceil(carvao / 5);
     const cerveja = (data.homens + data.mulheres) * 3;
-    
+
     enviarDados({
       ...data,
       id: uuidv4(),
@@ -76,7 +68,7 @@ export default function Formulario() {
 
   return (
     <div className="container">
-      <Navbar/>
+      <Navbar />
       <div className="content">
         <div className="box">
           <div className="left">
@@ -88,23 +80,45 @@ export default function Formulario() {
               <div className="formulario-inner">
                 <div className="field">
                   <label>Data:</label>
-                  <Input type="date" register={register} name="data" error={errors.data} />
+                  <Input
+                    type="date"
+                    register={register}
+                    name="data"
+                    error={errors.data}
+                  />
                 </div>
                 <div className="field">
                   <label>Homens:</label>
-                  <Input type="number" register={register} name="homens" error={errors.homens} />
+                  <Input
+                    type="number"
+                    register={register}
+                    name="homens"
+                    error={errors.homens}
+                  />
                 </div>
                 <div className="field">
                   <label>Mulheres:</label>
-                  <Input type="number" register={register} name="mulheres" error={errors.mulheres} />
+                  <Input
+                    type="number"
+                    register={register}
+                    name="mulheres"
+                    error={errors.mulheres}
+                  />
                 </div>
                 <div className="field">
                   <label className="label">Crianças:</label>
-                  <Input type="number" register={register} name="criancas" error={errors.criancas} />
+                  <Input
+                    type="number"
+                    register={register}
+                    name="criancas"
+                    error={errors.criancas}
+                  />
                 </div>
               </div>
               <div className="button-group">
-                <button type="submit" className="botao-enviar">Enviar</button>
+                <button type="submit" className="botao-enviar">
+                  Enviar
+                </button>
               </div>
             </form>
           </div>
