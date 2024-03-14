6e7c9f467b5456ad81e61ff89d3cd57e468bb0aa
Carolina Dutra <dutracarol001@gmail.com>
2024-03-14 18:16:33 -0300

    alguns ajustes do css

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import './navbar.css'
-import logo from '../../assets/logo.png'
-
-
+import './navbar.css';
+import logo from '../../assets/logo.png';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-    <div className="nav">
-        <div className="nav-logo">
-            <img src={logo} alt="Logo" />
+        <div className="nav">
+            <div className="nav-logo">
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className={`nav-menu ${menuOpen ? 'active' : ''}`}>
+                <ul>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/formulario"}>Nova Lista</Link></li>
+                    <li className='nav-newlist'><Link to={"/lista"}>Sua lista</Link></li>
+                </ul>
+            </div>
+            <div className="menu-toggle" onClick={toggleMenu}>
+                <div className="icon">{menuOpen ? 'X' : '☰'}</div>
+            </div>
         </div>
-            <ul className='nav-menu'>
-            <li><Link to={"/"}>Home</Link></li>
-            <li><Link to={"/formulario"}>Nova Lista</Link></li>
-            <li className='nav-newlist'><Link to={"/lista"}>Sua lista</Link></li>
-        </ul>
-    </div>
-    )
-}
+    );
+};
 
 export default Navbar;
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,25 +1,24 @@
 import { Link } from "react-router-dom";
-import './home.css'
+import './home.css';
 import Navbar from '../../components/Navbar/navbar';
-import arrow_btn from './../../assets/arrow_btn.png'
+import arrow_btn from './../../assets/arrow_btn.png';
 
 function Home() {
     return (
-    
-            <div className='start'>
-                <Navbar />
+        <div className='start'>
+            <Navbar />
+            <div className='start-content'>
                 <div className='start-text'>
-                <p>O cálculo ideal</p>
-                <p>para o seu churrasco</p>
-             </div>
-            <div className='star-button'>
-            <p><Link to={"/formulario"}>Comece criando aqui</Link></p>
-            <Link to={"/formulario"}><img src={arrow_btn} alt="" /></Link>
-
+                    <p>O cálculo ideal</p>
+                    <p>para o seu churrasco</p>
+                </div>
+                <div className='star-button'>
+                    <p><Link to={"/formulario"}>Comece criando aqui</Link></p>
+                    <Link to={"/formulario"}><img src={arrow_btn} alt="" /></Link>
+                </div>
             </div>
-         </div>
-
+        </div>
     );
 }
 
-export default Home; 
+export default Home;
